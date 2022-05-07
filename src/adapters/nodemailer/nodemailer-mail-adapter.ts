6a09a49de00de4f18f0fee59a33d27d30a4d055a
Jonathan <jonathan.riosousa@gmail.com>
2022-05-07 08:15:18 -0300

    first commit

---
+++ src/adapters/nodemailer/nodemailer-mail-adapter.ts
@@ -0,0 +1,22 @@
+import nodemailer from "nodemailer";
+import { MailAdapter, SendMailData } from "../mail-adapter";
+
+const transport = nodemailer.createTransport({
+  host: "smtp.mailtrap.io",
+  port: 2525,
+  auth: {
+    user: "fc6f14bb73810b",
+    pass: "024d0270ae112f",
+  },
+});
+
+export class NodemailerMailAdapter implements MailAdapter {
+  async sendMail({ subject, body }: SendMailData) {
+    await transport.sendMail({
+      from: "Equipe Feedget <oi@feedget.com>",
+      to: "Jonathan Rios <jonathan.riosousa@gmail.com.br>",
+      subject,
+      html: body,
+    });
+  }
+}
